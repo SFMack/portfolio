9c83c55eaa6c23da96efdeca24b0579e18c015ef
SFMack <sfcamack@icloud.com>
2020-03-31 17:56:51 -0700

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-0fb431b98b2cb1f37d76.js"
+    "url": "webpack-runtime-41b0af7ceb72125c7d28.js"
   },
   {
     "url": "framework-702fea457e693ce8cfdc.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "1c37acae690de9f41edae4e492027ad6"
+    "revision": "c8fbe4b00456315c6955a1a0119ee5bf"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-f5f5aeb76a7fcd2a6385.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "945593870a13005de9e483c064f9af7a"
+    "revision": "c025be245eecdfe185288f16f39cf1c6"
   },
   {
     "url": "manifest.webmanifest",
